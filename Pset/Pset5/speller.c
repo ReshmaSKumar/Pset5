da00285b2f1fcd2588e9bed19cea9286db2f6802
Reshma S.Kumar <gs_reji@yahoo.co.in>
2017-05-24 23:50:23 +0530

    Statements to ensure whether the usage is correctand whether text and dictionary exist

---
+++ Pset/Pset5/speller.c
@@ -0,0 +1,53 @@
+#include<ctype.h>
+#include<stdio.h>
+#include<sys/resource.h>
+#include<sys/time.h>
+
+#include "dictionary.h"
+
+#undef calculate
+#undef getrusage
+
+#define DICTIONARY "dictionaries/large"
+
+double calculate(const struct rusage* b,const struct rusage* a);
+
+int main(int argc,char* argv[])
+{
+	if(argc!=2 && argc!=3)
+	{
+		printf("Usage:speller [dictinary] text\n");
+		return 1;
+	}
+
+	struct rusage before,after;
+
+	double time_load=0.0,time_check=0.0,time_size=0.0,time_unload=0.0;
+
+	char* dictionary=(argc==3)?argv[1]:DICTIONARY;
+
+	getrusage(RUSAGE_SELF,&before);
+	bool loaded=load(dictionary);
+	getrusage(RUSAGE_SELF,&after);
+
+	if(!loaded)
+	{
+		printf("Could not load%s.\n",dictionary);
+		return 1;
+	}
+
+	 time_load=calculate(&before,&after);
+	 char* text=(argc==3)?argv[2]:argv[1];
+
+	FILE* fp=fopen(text,"r");
+	if(fp==NULL)
+	{
+		printf("Could not open%s.\n",text);
+		unload();
+		return 1;
+	}
+}
+
+
+
+
